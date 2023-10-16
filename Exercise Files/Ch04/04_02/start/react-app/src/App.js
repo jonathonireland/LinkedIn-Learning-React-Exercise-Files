60d085545bb1229b9ba22bac934c3dbf911e5f98
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-16 11:58:57 -0400

    adding exercise files

---
+++ Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const submit = (e) => {
+    const [title, setTitle] = useState("");
+    const [color, setColor] = useState("#000000");
+    const submit = (e) => {
+      e.preventDefault();
+      alert(`${title}, ${color}`);
+      setTitle("");
+      setColor("#000000");
+    }
+  };
+
+  return (
+      <form onSubmit={submit}>
+        <input 
+          value={title}
+          type="text"
+          onChange={
+            (event) => setTitle(event.target.value)
+          }
+          placeholder="color title..."
+        />
+        <input 
+          value={color}
+          type="color"
+          onChange={
+            (event) => 
+            setColor(event.target.value)
+          }
+        />
+        <button>ADD</button>
+      </form>
+  );
+}
+
+export default App;
