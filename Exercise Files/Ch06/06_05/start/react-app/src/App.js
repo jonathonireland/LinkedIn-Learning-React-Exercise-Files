fce788506bc9618116a95ab2f3125ae72028a109
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-17 11:19:40 -0400

    added routes and nested routes

--- Exercise Files/Ch06/06_05/start/react-app/src/App.js
+++ Exercise Files/Ch06/06_05/start/react-app/src/App.js
@@ -1,64 +1,32 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-    status
-  }
-}
-`;
-
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query })
-};
-
-function Lift({ name, elevationGain, status }) {
+function Home(){
   return (
     <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
+      <h1>My Website</h1>
+    </div>
+  );
+}
+
+function About(){
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+function Contact(){
+  return (
+    <div>
+      <h1>Contact</h1>
     </div>
   );
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      `https://snowtooth.moonhighway.com/`,
-      opts
-    )
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error)
-    return <pre>{JSON.stringify(error)}</pre>;
-  if (!data) return null;
   return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <HHome />
   );
 }
 
